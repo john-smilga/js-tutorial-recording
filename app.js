33c4bed9bc25ff1f703d883208a6a6f09eac644e
john-smilga <johnsmilga@gmail.com>
2019-12-07 07:54:09 -0800

    form submit

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-// allows select dynamic elements
-// event propogation - order the events are fired
-// event bubbling - clicked element first then bubbles up -- default
+// submit event listener
+// prevent default
+// how to get a value
 
-const container = document.querySelector('.container');
-const btn = document.querySelector('.btn');
-const heading = document.querySelector('.heading');
-console.log(heading);
-
-function sayHello() {
-  console.log('hello there');
-}
-
-btn.addEventListener('click', function() {
-  const element = document.createElement('h1');
-  element.classList.add('heading');
-  element.textContent = `i'm inside container`;
-  container.appendChild(element);
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  console.log('form submitted');
+  console.log(name.value);
+  console.log(password.value);
 });
-container.addEventListener('click', function(e) {
-  if (e.target.classList.contains('heading')) {
-    console.log('hello');
-  }
-});
-
-// heading.addEventListener('click', sayHello);
