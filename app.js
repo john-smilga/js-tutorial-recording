9184c70f541c060ed8e808ce2a8a4b1c628fadf3
john-smilga <johnsmilga@gmail.com>
2019-12-21 08:53:09 -0800

    array destructuring

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-// Arrow Functions or Fat Arrow Functions
-// 'this' keyword
+// Destructuring
+// faster/easier way to access/unpack values from arrays
+// objects into variables
+// Arrays
 
-const bob = {
-  firstName: "bob",
-  lastName: "smith",
-  sayName: function() {
-    console.log(this);
-    setTimeout(() => {
-      console.log(this);
-      console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
-    }, 1000);
-  },
-};
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
 
-const anna = {
-  firstName: "anna",
-  lastName: "sanders",
-  sayName: () => {
-    console.log(this);
-    console.log(`Hello, my name is ${this.firstName} ${this.lastName}`);
-  },
-};
+const orange = fruits[0];
+const banana = fruits[1];
+const lemon = fruits[2];
+console.log(orange, banana, lemon);
 
-bob.sayName();
-anna.sayName();
+const [john, , enemy, bob, susan] = friends;
+console.log(john, enemy, bob, susan);
