9f2aa8cb27b5f6a55db22668ee41f1dfe7afdf6c
john-smilga <johnsmilga@gmail.com>
2019-12-21 10:10:00 -0800

    Destructuring Function Parameters/Arguments

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Destructuring
 // faster/easier way to access/unpack values from arrays
 // objects into variables
-// Objects
+// As Function Arguments
 
 const bob = {
   first: 'bob',
@@ -12,17 +12,9 @@ const bob = {
   },
 };
 
-const {
-  first: firstName,
-  last,
-  city,
-  zip,
-  siblings: { sister: favoriteSibling },
-} = bob;
-console.log(firstName, last, city, zip, favoriteSibling);
+function printPerson({ first, last, city }) {
+  // const { first, last, city } = person;
+  console.log(first, last, city);
+}
 
-// const firstName = bob.first;
-// const lastName = bob.last;
-// const sister = bob.siblings.sister;
-
-// console.log(firstName, lastName, sister);
+printPerson(bob);
