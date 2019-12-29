7c28ac09f630d953bb0edde8539deca439be8874
john-smilga <johnsmilga@gmail.com>
2019-12-29 07:03:03 -0800

    Spread Operator With DOM Elements

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 // Allows an iterable to spread/expand individually inside reciever
 // Split into single items and copy them.
 
-const person = { name: 'john', job: 'developer' };
-const newPerson = { ...person, city: 'los angeles', name: 'peter' };
-console.log(newPerson);
+const headings = document.querySelectorAll('h1');
+const result = document.getElementById('result');
+const text = [...headings]
+  .map(item => `<span>${item.textContent}</span>`)
+  .join('');
+console.log(text);
+
+result.innerHTML = text;
