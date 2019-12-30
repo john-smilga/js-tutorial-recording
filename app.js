5a0ee4a6283669f240880c9ccab266827f87e965
john-smilga <johnsmilga@gmail.com>
2019-12-29 17:31:11 -0800

    Array.from - string and arguments examples

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // Array.from and Array.of - NOT ON THE PROTOTYPE
 
-// of creates a new Array instance from a variable number of arguments.
-
 // from - returns Array Object from any object with a length property or an iterable object
-// from turns array-like/ish into array - string,nodeList
+// from turns array-like/ish into array - string,nodeList,Set
 
-const example = ['one', 'two', 'three'];
-// console.log(example);
-// console.log(example.map);
-// console.log(example.from);
-// console.log(example.of);
+const udemy = 'udemy';
+// console.log(Array.from(udemy));
 
-const friends = Array.of('john', 1, true);
-// console.log(friends);
+function getTotal() {
+  // console.log(arguments);
+  // console.log(Array.from(arguments));
+  let total = Array.from(arguments).reduce(
+    (total, currNum) => (total += currNum),
+    0
+  );
+  console.log(total);
+}
+
+getTotal(10, 11, 24);
